f92116497adb64a924a9ef8dc98cf2d10b2e1495
Xiaoguo Yu <285726161@qq.com>
2018-02-09 17:28:03 +0800

    0.0.3

--- js/index.js
+++ js/index.js
@@ -4,12 +4,14 @@ var total=14;
 var imgInfo=new Array();
 var result=new Array();
 var begain=null;
+var shuliang=0;
+var list=new Array();
 
 
 
 $(document).ready(function(){
     
-	getSomeRandomNum(2)
+	
 
 
     
@@ -19,15 +21,37 @@ $(document).ready(function(){
 
 
 function begainImg(){
+    $('.checkBtn').attr('src','img/no.png');
+    var b=0,
+        tempId='';
+    for(var a=0;a<5;a++){
+        b=a+1;
+        tempId='#changeImg'+b;
+        if($(tempId).attr('src') == 'img/default.jpg'){
+            // console.log($(tempId).attr('src'))
+            continue;
+        }
+        console.log($(tempId).attr('src'));
+        list.push($(tempId).attr('src'));
+    }
+    console.log(list)
+    shuliang=list.length;
+    document.getElementById('personNum').innerText='('+shuliang+')';
+
+
     begain=window.setInterval(function(){
-        var i=Math.ceil(total*Math.random());
-        $('#changeImg').attr('src','img/'+imgInfo[i]);
-        result[0]=imgInfo[i];
+        var tempResult=getSomeRandomNum(5);
+        var imgId='';
+        for(var i=1;i<6;i++){
+            imgId='#changeImg'+i;
+            $(imgId).attr('src','img/'+tempResult[i-1]);
+        }
     },200);
 }
 
 function stopImg(){
     window.clearInterval(begain);
+    $('.checkBtn').attr('src','img/ok.png');
 }
 
 function getSomeRandomNum(num){
@@ -46,16 +70,15 @@ function getSomeRandomNum(num){
     	console.log(result)
     	temp3--;
     	temp=Math.ceil(temp3*Math.random());
-    	console.log(temp)
+    	// console.log(temp)
     	resultArray[k]=result[temp];
     	temp2=result[0];
     	result[0]=result[temp];
     	result[temp]=temp2;
     	result.shift();
-//  	temp3--;
     }
-    console.log(resultArray)
-    
+    // console.log(resultArray);
+    return resultArray;
 }
 
 function readFiles(){
@@ -67,4 +90,20 @@ function readFiles(){
         imgInfo[j]=files[j].name;
     }
     console.log(imgInfo);
+    $('#selectArea').css('display','none');
+}
+
+function checkResult(event){
+    var confirmResult=window.confirm('取消后将无法撤回，确定吗？');
+    if(confirmResult){
+        event.target.src='img/no.png';
+        var thisId=event.target.id[11];
+        $('#changeImg'+thisId).attr('src','img/default.jpg');
+
+    }
+}
+
+
+function deleteItem(item,array){
+    
 }
